6e08da9764f9ad4c6df9b2cf6c9bd6d09360b28d
Marcus Carter <marcoose408@gmail.com>
2025-07-06 17:08:43 -0400

    indices / element buffer object

--- src/main.c
+++ src/main.c
@@ -31,6 +31,15 @@ int main()
         -0.5f, -0.5f * (float)(sqrt(3)) / 3, 0.0f,
          0.5f, -0.5f * (float)(sqrt(3)) / 3, 0.0f,
          0.0f,  0.5f * (float)(sqrt(3)) * 2 / 3, 0.0f,
+        -0.5f / 2, 0.5f * (float)(sqrt(3)) / 6, 0.0f,
+         0.5f / 2,  0.5f * (float)(sqrt(3)) / 6, 0.0f,
+         0.0f, -0.5f * (float)(sqrt(3)) / 3, 0.0f,
+    };
+
+    GLuint indices[] = {
+        0, 3, 5, 
+        3, 2, 4,
+        5, 4, 1,
     };
 
     GLFWwindow* window = glfwCreateWindow(800, 800, "opengl window", NULL, NULL);
@@ -66,21 +75,27 @@ int main()
 
     // BUFFERS
 
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     
 
 
@@ -102,7 +117,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         glfwPollEvents();
@@ -110,6 +125,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
     glfwTerminate();
