54691adaaf2e1f5428cc34501f27191e617364a4
Marcus Carter <marcoose408@gmail.com>
2025-08-04 23:02:43 -0400

    test scene

--- include/opengl/error.h
+++ include/opengl/error.h
@@ -5,7 +5,7 @@
 
 #define MSG_ERROR(file, line, msg, ...)    fprintf(stderr, "%s:%d:\033[31m error:\033[0m " msg "\n", file, line, ##__VA_ARGS__)
 #define MSG_WARNING(file, line, msg, ...)  fprintf(stderr, "%s:%d:\033[35m warning:\033[0m " msg "\n", file, line, ##__VA_ARGS__)
-#define MSG_FATAL(file, line, msg, ...)    do { fprintf(stderr, "%s:%d:\033[91m fatal:\033[0m " msg "\n", file, line, ##__VA_ARGS__); exit(1); } while (0)
+#define MSG_FATAL(file, line, msg, ...)    do { fprintf(stderr, "%s:%d:\033[91m fatal error:\033[0m " msg "\n", file, line, ##__VA_ARGS__); exit(1); } while (0)
 #define MSG_INFO(file, line, msg, ...)     fprintf(stdout, "%s:%d:\033[37m info:\033[0m " msg "\n", file, line, ##__VA_ARGS__)
 
 #endif
